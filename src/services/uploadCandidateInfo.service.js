8f370c9b2c7257f09e59837bfa31f1181927d026
shashwotbhattarai <shashwot.tech@gmail.com>
2024-04-23 18:51:52 +0545

    error

--- src/services/uploadCandidateInfo.service.js
+++ src/services/uploadCandidateInfo.service.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { ROUTES } from "../config/routes/constants";
 export async function uploadCandidateInfo(formData, token) {
   try {
+    let s3BadBucketUploadResponse;
     if (formData.cv !== null) {
       const key = Date.now() + "_" + formData.cv.name;
       const urlResponse = await axios.get(
@@ -18,6 +19,29 @@ export async function uploadCandidateInfo(formData, token) {
           "Content-Type": "multipart/form-data",
         },
       });
+
+      while (s3uploadResponse.status !== 200) {
+        const badBucketUrlResponse = await axios.get(
+          `${ROUTES.CANDIDATE_MICROSERVICE_URL}/badbucketUploadURL`,
+          {
+            headers: { Authorization: `Bearer ${token}`, key: key },
+          },
+        );
+
+        const badbucketUploadURL = badBucketUrlResponse.data.url;
+
+        s3BadBucketUploadResponse = await axios.put(
+          badbucketUploadURL,
+          formData.cv,
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          },
+        );
+
+        break;
+      }
       const data = {
         fullname: formData.fullname,
         email: formData.email,
@@ -33,6 +57,16 @@ export async function uploadCandidateInfo(formData, token) {
         return {
           status: 200,
           message: "Candidate info uploaded successfully",
+        };
+      } else if (
+        s3BadBucketUploadResponse.status === 200 &&
+        response.status === 200
+      ) {
+        ///TODO : Need to send email to user to inform that CV upload has failed, but has been uploaded to bad bucket.
+        return {
+          status: 500,
+          message:
+            "CV uplaod has failed. But uploaded to bad bucket, Pleae try again later",
         };
       } else {
         return {
